5c6cb08ceb27b81e2b7bdea77c149f03a2106dc1
mohammadgdz <mhd.gdz@gmail.com>
2023-07-07 14:30:29 +0330

    Initial commit

---
+++ hustle-mode/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  hustle-mode
+//
+//  Created by Mohammad on 2023-07-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var darkBlueBg: UIImageView!
+    @IBOutlet weak var powerBtn: UIButton!
+    @IBOutlet weak var cloudHolder: UIView!
+    @IBOutlet weak var rocket: UIImageView!
+    @IBOutlet weak var hustlLbl: UILabel!
+    @IBOutlet weak var onLbl: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
